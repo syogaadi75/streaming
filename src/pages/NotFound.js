ecb8210b942449801340a74e12cfb9ffafd9f9b3
Yoga Adi Saputra <syogaadi75@gmail.com>
2022-09-27 10:11:02 +0700

    Change Not Found Error

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -9,7 +9,7 @@ function NotFound() {
     return (
         <div className='flex w-full h-screen justify-center items-center flex-col'>
             <h1 className='text-4xl lg:text-6xl font-bold text-primary'>404, ERROR PAGE</h1>
-            <img className='w-80' src="https://firebasestorage.googleapis.com/v0/b/y-stream-a09e6.appspot.com/o/error.png?alt=media&token=772e13d9-97e5-44db-a675-8b4323cef6eb" alt="Not Found" />
+            <img className='w-80' src="https://archive.org/download/error_202209/error.png" alt="Not Found" />
             <div className='flex flex-col'>
                 <div className='mb-4'>
                     <span className='font-bold'>Oops!</span>, Halaman ini tidak tersedia
