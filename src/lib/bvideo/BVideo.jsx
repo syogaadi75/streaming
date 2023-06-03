a2f7cf9ecc1d7ea9e5cc3e129ba3df045063ac86
Yoga Adi Saputra <syogaadi75@gmail.com>
2023-06-04 00:30:29 +0700

    add server

--- src/lib/bvideo/BVideo.jsx
+++ src/lib/bvideo/BVideo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function BVideo() {
+function BVideo({ videoSource }) {
     return (
         <>
-            <iframe className='w-[500px] lg:w-[620px] h-[180px] lg:h-[350px]' title="animey" id="mediaplayer" src="https://www.blogger.com/video.g?token=AD6v5dwx_909t5EM66sA31gpqOoBfXpmfME8EnsM-2a1xPXoEGcB3UJtwH4JKePLZhVRbGBLB04fJguLXVELeiEP486M_5U3aEfsYTB5Ku7eh8W5tDQ9wh4x0VNuYt3pEtIwfOkxlhJ_" allowFullScreen={true} frameBorder="0" marginWidth="0" marginHeight="0" scrolling="NO"></iframe>
+            <iframe className='w-[500px] lg:w-[620px] h-[180px] lg:h-[350px]' title="animey" id="mediaplayer" src={videoSource} allowFullScreen={true} frameBorder="0" marginWidth="0" marginHeight="0" scrolling="NO"></iframe>
         </>
     )
 }
